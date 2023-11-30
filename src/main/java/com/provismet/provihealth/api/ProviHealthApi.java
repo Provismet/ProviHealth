e045ae635d056288cc50b4925f7077b14ab2e642
Provismet <17149901+Provismet@users.noreply.github.com>
2023-11-30 03:34:59 +0000

    Add ability to make custom portraits per entity.

--- src/main/java/com/provismet/provihealth/api/ProviHealthApi.java
+++ src/main/java/com/provismet/provihealth/api/ProviHealthApi.java
@@ -1,5 +1,78 @@
 package com.provismet.provihealth.api;
 
+import com.provismet.provihealth.hud.BorderRegistry;
+
+import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
+
 public interface ProviHealthApi {
     public void onInitialize ();
+
+    /**
+     * Registers an icon that will display on top of the health bar in the HUD for a specific entity group.
+     * Only one icon may be registered per entity group.
+     * 
+     * The icon takes the form of an item.
+     * 
+     * @param entityGroup The entity group.
+     * @param item The item to serve as the icon.
+     */
+    public default void registerIcon (EntityGroup entityGroup, Item item) {
+        BorderRegistry.registerItem(entityGroup, item);
+    }
+
+    public default boolean registerIcon (EntityType<?> type, Item item) {
+        return this.registerIcon(type, item, false);
+    }
+
+    /**
+     * Sets an icon to appear on the HUD for a certain entity type. 
+     * If available, icons for entity types will suppress icons for entity groups.
+     * 
+     * Only one icon can be registered per entity type. This registry may suppress or be suppressed by other mods.
+     * 
+     * @param type The entity type.
+     * @param item The item that will act as the icon for this entity type.
+     * @param force Whether or not this registration should override any pre-existing icon for the same entity type.
+     * @return Whether or not the registry succeeded. Expect this to be true when force = true.
+     */
+    public default boolean registerIcon (EntityType<?> type, Item item, boolean force) {
+        return BorderRegistry.registerItem(type, item.getDefaultStack(), force);
+    }
+
+    /**
+     * Registers a portrait for an entity group. This will affect the HUD health bar.
+     * Only one portrait may be registered per entity group.
+     * 
+     * If your mod introduces new entity groups, use this method to define a portrait for them.
+     * 
+     * The image for the frame must be 96x48 in size. With the foreground (48x48) on the left and the background (48x48) on the right.
+     * 
+     * @param entityGroup The entity group.
+     * @param resource A full resource path (modid:textures/gui/etc/file.png) to the texture.
+     */
+    public default void registerPortrait (EntityGroup entityGroup, Identifier resource) {
+        BorderRegistry.registerBorder(entityGroup, resource);
+    }
+
+    public default boolean registerPortrait (EntityType<?> type, Identifier resource) {
+        return this.registerPortrait(type, resource, false);
+    }
+
+    /**
+     * Sets the HUD portrait frame (imagery around the paper doll) for a certain entity type.
+     * If available, a portrait for an entity type will suppress the portrait for an entity group.
+     * 
+     * Only one portrait can be registered per entity type. This registry may suppress or be suppressed by other mods.
+     * 
+     * @param type The entity type.
+     * @param resource A full resource path (modid:textures/gui/etc/file.png) to the texture.
+     * @param force Whether or not this registration should override any pre-existing portrait for the same entity type.
+     * @return Whether or not the registry succeeded. Expect this to be true when force = true.
+     */
+    public default boolean registerPortrait (EntityType<?> type, Identifier resource, boolean force) {
+        return BorderRegistry.registerBorder(type, resource, force);
+    }
 }
--- src/main/java/com/provismet/provihealth/hud/BorderRegistry.java
+++ src/main/java/com/provismet/provihealth/hud/BorderRegistry.java
@@ -5,8 +5,11 @@ import java.util.HashMap;
 import org.jetbrains.annotations.Nullable;
 
 import com.provismet.provihealth.ProviHealthClient;
+import com.provismet.provihealth.config.Options;
 
 import net.minecraft.entity.EntityGroup;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -14,33 +17,73 @@ import net.minecraft.util.Identifier;
 public class BorderRegistry {
     private static final HashMap<EntityGroup, Identifier> groupBorders = new HashMap<>();
     private static final HashMap<EntityGroup, ItemStack> groupItems = new HashMap<>();
+    private static final HashMap<EntityType<?>, Identifier> overrideBorders = new HashMap<>();
+    private static final HashMap<EntityType<?>, ItemStack> overrideItems = new HashMap<>();
 
     private static final Identifier DEFAULT = ProviHealthClient.identifier("textures/gui/healthbars/default.png");
 
-    public static void registerEntityGroupBorder (EntityGroup group, Identifier border) {
+    public static void registerBorder (EntityGroup group, Identifier border) {
         if (group == null || border == null) ProviHealthClient.LOGGER.error("Attempted to register a null object to the border registry.");
         else if (groupBorders.containsKey(group)) ProviHealthClient.LOGGER.error("Attempted to register an already registered entity group.");
         else groupBorders.put(group, border);
     }
 
     public static void registerItem (EntityGroup group, ItemStack item) {
-        if (group == null || item == null) ProviHealthClient.LOGGER.error("Attempted to register a null object to the item registry.");
+        if (group == null || item == null) ProviHealthClient.LOGGER.error("Attempted to register a null object to the icon registry.");
         else if (groupItems.containsKey(group)) ProviHealthClient.LOGGER.error("Attempted to register an already registered entity group.");
         else groupItems.put(group, item);
+    }
+
+    public static boolean registerBorder (EntityType<?> type, Identifier border, boolean force) {
+        if (type == null || border == null) {
+            ProviHealthClient.LOGGER.error("Attempted to register a null object to the border registry.");
+            return false;
+        }
+        else {
+            if (overrideBorders.containsKey(type)) {
+                if (force) {
+                    ProviHealthClient.LOGGER.warn("The border for " + type.getName().getString() + " has been overridden.");
+                }
+                else return false;
+            }
+            overrideBorders.put(type, border);
+            return true;
+        }
+    }
+
+    public static boolean registerItem (EntityType<?> type, ItemStack item, boolean force) {
+        if (type == null || item == null) {
+            ProviHealthClient.LOGGER.error("Attempted to register a null object to the icon registry.");
+            return false;
+        }
+        else {
+            if (overrideItems.containsKey(type)) {
+                if (force) {
+                    ProviHealthClient.LOGGER.warn("The icon for " + type.getName().getString() + " has been overridden.");
+                }
+                else return false;
+            }
+            overrideItems.put(type, item);
+            return true;
+        }
     }
 
     public static void registerItem (EntityGroup group, Item item) {
         registerItem(group, item.getDefaultStack());
     }
 
-    public static Identifier getBorder (@Nullable EntityGroup group) {
-        if (group == null || !groupBorders.containsKey(group)) return DEFAULT;
-        else return groupBorders.get(group);
+    public static Identifier getBorder (@Nullable LivingEntity entity) {
+        if (entity == null || !Options.useCustomHudPortraits) return DEFAULT;
+        else if (overrideBorders.containsKey(entity.getType())) return overrideBorders.get(entity.getType());
+        else if (groupBorders.containsKey(entity.getGroup())) return groupBorders.get(entity.getGroup());
+        else return DEFAULT;
     }
 
     @Nullable
-    public static ItemStack getItem (EntityGroup group) {
-        if (group == null || !groupItems.containsKey(group)) return null;
-        else return groupItems.get(group);
+    public static ItemStack getItem (LivingEntity entity) {
+        if (entity == null) return null;
+        else if (overrideItems.containsKey(entity.getType())) return overrideItems.get(entity.getType());
+        else if (groupItems.containsKey(entity.getGroup())) return groupItems.get(entity.getGroup());
+        else return null;
     }
 }
