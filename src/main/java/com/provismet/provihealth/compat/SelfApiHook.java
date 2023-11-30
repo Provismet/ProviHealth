1c3ab6652ca758841d72c01f22969424f0347da7
Provismet <17149901+Provismet@users.noreply.github.com>
2023-11-30 03:42:36 +0000

    Improve and finish textures.

--- src/main/java/com/provismet/provihealth/compat/SelfApiHook.java
+++ src/main/java/com/provismet/provihealth/compat/SelfApiHook.java
@@ -1,7 +1,7 @@
 package com.provismet.provihealth.compat;
 
+import com.provismet.provihealth.ProviHealthClient;
 import com.provismet.provihealth.api.ProviHealthApi;
-import com.provismet.provihealth.hud.BorderRegistry;
 
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.item.Items;
@@ -9,10 +9,15 @@ import net.minecraft.item.Items;
 public class SelfApiHook implements ProviHealthApi {
     @Override
     public void onInitialize () {
-        BorderRegistry.registerItem(EntityGroup.AQUATIC, Items.COD);
-        BorderRegistry.registerItem(EntityGroup.ARTHROPOD, Items.COBWEB);
-        BorderRegistry.registerItem(EntityGroup.ILLAGER, Items.IRON_AXE);
-        BorderRegistry.registerItem(EntityGroup.UNDEAD, Items.ROTTEN_FLESH);
+        this.registerIcon(EntityGroup.AQUATIC, Items.COD);
+        this.registerIcon(EntityGroup.ARTHROPOD, Items.COBWEB);
+        this.registerIcon(EntityGroup.ILLAGER, Items.IRON_AXE);
+        this.registerIcon(EntityGroup.UNDEAD, Items.ROTTEN_FLESH);
+
+        this.registerPortrait(EntityGroup.AQUATIC, ProviHealthClient.identifier("textures/gui/healthbars/aquatic.png"));
+        this.registerPortrait(EntityGroup.ARTHROPOD, ProviHealthClient.identifier("textures/gui/healthbars/arthropod.png"));
+        this.registerPortrait(EntityGroup.ILLAGER, ProviHealthClient.identifier("textures/gui/healthbars/illager.png"));
+        this.registerPortrait(EntityGroup.UNDEAD, ProviHealthClient.identifier("textures/gui/healthbars/undead.png"));
     }
     
 }
