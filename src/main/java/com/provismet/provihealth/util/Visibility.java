e6fee01c8fffbc5b37bbe5c7c214705ce532a76c
Provismet <17149901+Provismet@users.noreply.github.com>
2023-12-10 20:25:11 +0000

    Remove experimental DisplayEntity code.

--- src/main/java/com/provismet/provihealth/util/Visibility.java
+++ src/main/java/com/provismet/provihealth/util/Visibility.java
@@ -1,11 +1,8 @@
 package com.provismet.provihealth.util;
 
-import com.provismet.provihealth.config.Options;
-
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.decoration.DisplayEntity;
 
 public class Visibility {
     @SuppressWarnings("resource")
@@ -15,7 +12,6 @@ public class Visibility {
         if (!living.getEquippedStack(EquipmentSlot.HEAD).isEmpty()) return true;
         if (!living.getEquippedStack(EquipmentSlot.CHEST).isEmpty()) return true;
         if (living.isGlowing()) return true;
-        if (living.getWorld().getEntitiesByClass(DisplayEntity.class, living.getBoundingBox().expand(Options.boxSize), x -> true).size() > 0) return true;
         return false;
     }
 }
