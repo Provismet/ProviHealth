a342569fa60ca61ce8f0f8475217c7a546643dda
Provismet <17149901+Provismet@users.noreply.github.com>
2024-02-12 06:35:16 +0000

    Spectating players can never be visible.

--- src/main/java/com/provismet/provihealth/util/Visibility.java
+++ src/main/java/com/provismet/provihealth/util/Visibility.java
@@ -3,11 +3,13 @@ package com.provismet.provihealth.util;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 
 public class Visibility {
     @SuppressWarnings("resource")
     public static boolean isVisible (LivingEntity living) {
         if (!living.isInvisibleTo(MinecraftClient.getInstance().player)) return true;
+        if (living instanceof PlayerEntity player && player.isSpectator()) return false;
         if (living.hasPassengers()) return true;
         if (!living.getEquippedStack(EquipmentSlot.HEAD).isEmpty()) return true;
         if (!living.getEquippedStack(EquipmentSlot.CHEST).isEmpty()) return true;
