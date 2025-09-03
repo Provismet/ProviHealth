de70928dd5b072ac4ec39755c5b31b8f53a57917
Provismet <17149901+Provismet@users.noreply.github.com>
2025-09-03 23:14:24 +0100

    Remove unused shadow variable.

--- src/main/java/com/provismet/provihealth/mixin/EntityRendererMixin.java
+++ src/main/java/com/provismet/provihealth/mixin/EntityRendererMixin.java
@@ -8,7 +8,6 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.entity.state.EntityRenderState;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -17,7 +16,6 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.provismet.provihealth.config.Options;
-import com.provismet.provihealth.hud.TargetHealthBar;
 import com.provismet.provihealth.world.EntityHealthBar;
 
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -37,9 +35,6 @@ public abstract class EntityRendererMixin {
 
     @Shadow
     protected abstract boolean hasLabel (Entity entity, double squaredDistanceToCamera);
-
-    @Shadow
-    protected abstract @Nullable Text getDisplayName (Entity entity);
 
     @Inject(method="renderLabelIfPresent", at=@At("HEAD"), cancellable=true)
     private void cancelLabel (EntityRenderState state, Text text, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo info) {
